00d74a74cb03075bfd00cb236993f60290f69893
umutafacan <umut-afacan@hotmail.com>
2015-03-02 21:44:31 +0200

    Initial Commit

---
+++ SelfieKeyboard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfieKeyboard
+//
+//  Created by umut on 02/03/15.
+//  Copyright (c) 2015 Umut Afacan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelfieKeyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfieKeyboard
+//
+//  Created by umut on 02/03/15.
+//  Copyright (c) 2015 Umut Afacan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
